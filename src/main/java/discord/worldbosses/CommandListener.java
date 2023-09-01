c4504d57660b5e11c0f459d577b4e600e743d4da
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-09-01 21:06:14 +0200

    Timer table redesigned

--- src/main/java/discord/worldbosses/CommandListener.java
+++ src/main/java/discord/worldbosses/CommandListener.java
@@ -1,5 +1,6 @@
 package discord.worldbosses;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -16,6 +17,10 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 public class CommandListener extends ListenerAdapter {
     private BossManager bossManager = new BossManager();
     private Map<String, String> userStates = new HashMap<>();
@@ -28,61 +33,73 @@ public class CommandListener extends ListenerAdapter {
         // Schedule the task to run every 5 minutes
         scheduler.scheduleAtFixedRate(this::updateTimers, 0, 5, TimeUnit.MINUTES);
     }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         // Check if the message is sent by a bot. If yes, return immediately.
         if (event.getAuthor().isBot()) {
             return;
         }
-    
+
         this.jda = event.getJDA();
         String message = event.getMessage().getContentRaw();
         String userId = event.getAuthor().getId();
-    
+
         if (message.startsWith("!setDesignatedChannel")) {
             if (event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
                 designatedChannelId = event.getChannel().getId();
                 event.getChannel().sendMessage("This channel is now set as the designated channel for timers.")
-                    .queue(response -> {
-                        response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
-                    });
+                        .queue(response -> {
+                            response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
+                        });
             } else {
                 event.getChannel().sendMessage("You need administrator permissions to set the designated channel.")
-                    .queue(response -> {
-                        response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
-                    });
+                        .queue(response -> {
+                            response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
+                        });
             }
             event.getMessage().delete().queue(); // Delete user's command message
         } else if (message.startsWith("!addtimer")) {
-            if (event.getChannel().getId().equals(designatedChannelId)) { // Check if the command is in the designated channel
+            if (event.getChannel().getId().equals(designatedChannelId)) { // Check if the command is in the designated
+                                                                          // channel
                 SelectMenu menu = createMapSelectMenu();
                 event.getChannel().sendMessage("Please select a map from the dropdown.")
-                    .setActionRow(menu)
-                    .queue(response -> {
-                        response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
-                    });
+                        .setActionRow(menu)
+                        .queue(response -> {
+                            response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
+                        });
                 userStates.put(userId, "awaiting_map_selection");
             } else {
                 event.getChannel().sendMessage("This command can only be used in the designated channel.")
-                    .queue(response -> {
-                        response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
-                    });
+                        .queue(response -> {
+                            response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
+                        });
             }
             event.getMessage().delete().queue(); // Delete user's command message
         } else if (userStates.getOrDefault(userId, "").equals("awaiting_time_input")) {
             String time = message;
-            bossManager.addTimer(userStates.get(userId + "_selected_map"), time);
-            event.getChannel().sendMessage("Timer added for " + userStates.get(userId + "_selected_map") + " at " + time)
-                .queue(response -> {
-                    response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
-                });
+
+            // Get the date two days from now
+            LocalDate twoDaysLater = LocalDate.now().plusDays(2);
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+            String formattedDate = twoDaysLater.format(dateFormatter);
+
+            // Append the date to the time
+            String fullTime = time + " " + formattedDate;
+
+            bossManager.addTimer(userStates.get(userId + "_selected_map"), fullTime);
+            event.getChannel()
+                    .sendMessage("Timer added for " + userStates.get(userId + "_selected_map") + " at " + fullTime)
+                    .queue(response -> {
+                        response.delete().queueAfter(10, TimeUnit.SECONDS); // Delete bot's message after 10 seconds
+                    });
             userStates.remove(userId);
             userStates.remove(userId + "_selected_map");
             sendTimersToChannel();
             event.getMessage().delete().queue(); // Delete user's command message
         }
-    }    
-    
+
+    }
 
     @Override
     public void onGenericInteractionCreate(GenericInteractionCreateEvent event) {
@@ -90,15 +107,15 @@ public class CommandListener extends ListenerAdapter {
         if (event.getInteraction() instanceof SelectMenuInteraction<?, ?>) {
             SelectMenuInteraction<?, ?> selectMenu = (SelectMenuInteraction<?, ?>) event.getInteraction();
             String userId = selectMenu.getUser().getId();
-    
-            String customId = selectMenu.getComponentId(); 
-    
+
+            String customId = selectMenu.getComponentId();
+
             if (customId.equals("map-selector") && "awaiting_map_selection".equals(userStates.get(userId))) {
                 String selectedMap = (String) selectMenu.getValues().get(0);
                 selectMenu.getChannel().sendMessage("You selected " + selectedMap + ". Please provide the time.")
-                    .queue(response -> {
-                        response.delete().queueAfter(5, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
-                    });
+                        .queue(response -> {
+                            response.delete().queueAfter(5, TimeUnit.SECONDS); // Delete bot's message after 5 seconds
+                        });
                 userStates.put(userId, "awaiting_time_input");
                 userStates.put(userId + "_selected_map", selectedMap);
             }
@@ -136,29 +153,31 @@ public class CommandListener extends ListenerAdapter {
         TextChannel designatedChannel = jda.getTextChannelById(designatedChannelId);
         if (designatedChannel == null) return; // Designated channel not found
 
-        StringBuilder timersTable = new StringBuilder("Timers:\n");
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("World Boss Timers");
+        embed.setColor(0x00FFFF);
         for (Map.Entry<String, String> entry : bossManager.getAllTimers().entrySet()) {
-            timersTable.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            embed.addField(entry.getKey(), entry.getValue(), true);
         }
 
         if (timerMessageId == null) {
             // If the timer message hasn't been sent yet, send it
-            designatedChannel.sendMessage(timersTable.toString()).queue(message -> {
+            designatedChannel.sendMessageEmbeds(embed.build()).queue(message -> {
                 timerMessageId = message.getId();
             });
         } else {
             // If the timer message has already been sent, edit it
-            designatedChannel.editMessageById(timerMessageId, timersTable.toString()).queue();
+            designatedChannel.editMessageEmbedsById(timerMessageId, embed.build()).queue();
         }
     }
-
     private void updateTimers() {
         if (timerMessageId != null) {
             TextChannel channel = jda.getTextChannelById(designatedChannelId);
             if (channel != null) {
                 StringBuilder timersTable = new StringBuilder("Timers:\n");
                 for (Map.Entry<String, String> entry : bossManager.getAllTimers().entrySet()) {
-                    timersTable.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+                    timersTable.append("**").append(entry.getKey()).append("**: `").append(entry.getValue())
+                            .append("`\n");
                 }
                 channel.editMessageById(timerMessageId, timersTable.toString()).queue();
             }
