f2fc7a01520aa4b61f72f3600ecd6ddad46c50bd
Veyg <dawid.grochowski@veyg.me>
2023-10-02 08:30:12 +0200

    Dependabot + stream.collector deletion

--- src/main/java/discord/worldbosses/CommandListener.java
+++ src/main/java/discord/worldbosses/CommandListener.java
@@ -25,7 +25,6 @@ import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import discord.worldbosses.BossManager.TimerData;
 
