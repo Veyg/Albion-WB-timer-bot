4fcd830ee250a07ad85bd315497d53c9c9f4c167
Veyg <dawid.grochowski@veyg.me>
2023-10-05 19:37:51 +0200

    Bot token moved to config.json

--- src/main/java/discord/worldbosses/AlbionBot.java
+++ src/main/java/discord/worldbosses/AlbionBot.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.JDA;
 
 public class AlbionBot {
     public static void main(String[] args) throws Exception {
-        String token = "MTE0NTY3MTY3NjkwMjc4NTA4NA.GqwMqw.R8ZKeByKpZX4UIp0oqch9msZ8G74lSXFdK0MEA"; // Replace with your bot token
+        String token = ConfigManager.getBotToken();
         JDABuilder builder = JDABuilder.createDefault(token);
         builder.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT);
         builder.setActivity(Activity.playing("Infactor needs regears for ZvZ"));
