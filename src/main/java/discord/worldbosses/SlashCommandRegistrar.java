130758c2c1f44667e674e9880e4e4a18f46df194
Dawid Grochowski <dawid.grochowski@veyg.me>
2023-11-05 17:25:53 +0100

    Refractoring

--- src/main/java/discord/worldbosses/SlashCommandRegistrar.java
+++ src/main/java/discord/worldbosses/SlashCommandRegistrar.java
@@ -4,115 +4,57 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SlashCommandRegistrar {
 
-        private JDA jda;
+    private static final Logger logger = LoggerFactory.getLogger(SlashCommandRegistrar.class);
+    private JDA jda;
 
-        public SlashCommandRegistrar(JDA jda) {
-                this.jda = jda;
-                registerCommands();
+    public SlashCommandRegistrar(JDA jda) {
+        this.jda = jda;
+        registerCommands();
+    }
+
+    private OptionData getMapOptionData() {
+        OptionData mapOption = new OptionData(OptionType.STRING, "map", "Name of the map", true);
+        // Add all the map choices here
+        String[] maps = {
+            "Darkground Swamp", "Deadvein Gully", "Deathwisp Sink", "Drownfield Wetland", "Dryvein Steppe",
+            "Farshore Heath", "Flimmerair Steppe", "Hightree Levee", "Longfen Arms", "Longfen Veins",
+            "Longtimber Glen", "Nightbloom Forest", "Rivercopse Fount", "Runnelvein Bog", "Skysand Ridge",
+            "Stumprot Swamp", "Sunfang Cliffs", "Sunfang Dawn", "Sunstrand Dunes", "Timberslope Grove",
+            "Timbertop Dale", "Watchwood Bluffs", "Westweald Shore"
+        };
+        for (String map : maps) {
+            mapOption.addChoice(map, map);
         }
+        return mapOption;
+    }
 
-        public void registerCommands() {
-                // Registering the setDesignatedChannel command
-                jda.updateCommands().addCommands(
-                                Commands.slash("aboutme", "Get information about the bot"),
-                                Commands.slash("setdesignatedchannel", "Set the designated channel for timers"),
-                                Commands.slash("help", "Get a list of available commands and their descriptions"),
-                                Commands.slash("addtimer", "Add a timer for a world boss")
-                                                .addOption(OptionType.STRING, "time",
-                                                                "Time for the boss spawn in HH:mm:ss format", true)
-                                                .addOptions(
-                                                                new OptionData(OptionType.STRING, "map",
-                                                                                "Name of the map", true)
-                                                                                .addChoice("Darkground Swamp", "Darkground Swamp")
-                                                                                .addChoice("Deadvein Gully", "Deadvein Gully")
-                                                                                .addChoice("Deathwisp Sink", "Deathwisp Sink")
-                                                                                .addChoice("Drownfield Wetland", "Drownfield Wetland")
-                                                                                .addChoice("Dryvein Steppe", "Dryvein Steppe")
-                                                                                .addChoice("Farshore Heath", "Farshore Heath")
-                                                                                .addChoice("Flimmerair Steppe", "Flimmerair Steppe")
-                                                                                .addChoice("Hightree Levee", "Hightree Levee")
-                                                                                .addChoice("Longfen Arms", "Longfen Arms")
-                                                                                .addChoice("Longfen Veins", "Longfen Veins")
-                                                                                .addChoice("Longtimber Glen", "Longtimber Glen")
-                                                                                .addChoice("Nightbloom Forest", "Nightbloom Forest")
-                                                                                .addChoice("Rivercopse Fount", "Rivercopse Fount")
-                                                                                .addChoice("Runnelvein Bog", "Runnelvein Bog")
-                                                                                .addChoice("Skysand Ridge", "Skysand Ridge")
-                                                                                .addChoice("Stumprot Swamp", "Stumprot Swamp")
-                                                                                .addChoice("Sunfang Cliffs", "Sunfang Cliffs")
-                                                                                .addChoice("Sunfang Dawn", "Sunfang Dawn")
-                                                                                .addChoice("Sunstrand Dunes", "Sunstrand Dunes")
-                                                                                .addChoice("Timberslope Grove", "Timberslope Grove")
-                                                                                .addChoice("Timbertop Dale", "Timbertop Dale")
-                                                                                .addChoice("Watchwood Bluffs", "Watchwood Bluffs")
-                                                                                .addChoice("Westweald Shore", "Westweald Shore"))
-                                                .addOption(OptionType.STRING, "date", "Date for the boss spawn in dd/MM/yyyy format", false)
-                                                .addOption(OptionType.STRING, "note", "A note about the timer", false),
-                                Commands.slash("deletetimer", "Delete a timer for a world boss")
-                                                .addOptions(
-                                                                new OptionData(OptionType.STRING, "map",
-                                                                                "Name of the map", true)
-                                                                                .addChoice("Darkground Swamp", "Darkground Swamp")
-                                                                                .addChoice("Deadvein Gully", "Deadvein Gully")
-                                                                                .addChoice("Deathwisp Sink", "Deathwisp Sink")
-                                                                                .addChoice("Drownfield Wetland", "Drownfield Wetland")
-                                                                                .addChoice("Dryvein Steppe", "Dryvein Steppe")
-                                                                                .addChoice("Farshore Heath", "Farshore Heath")
-                                                                                .addChoice("Flimmerair Steppe", "Flimmerair Steppe")
-                                                                                .addChoice("Hightree Levee", "Hightree Levee")
-                                                                                .addChoice("Longfen Arms", "Longfen Arms")
-                                                                                .addChoice("Longfen Veins", "Longfen Veins")
-                                                                                .addChoice("Longtimber Glen", "Longtimber Glen")
-                                                                                .addChoice("Nightbloom Forest", "Nightbloom Forest")
-                                                                                .addChoice("Rivercopse Fount", "Rivercopse Fount")
-                                                                                .addChoice("Runnelvein Bog", "Runnelvein Bog")
-                                                                                .addChoice("Skysand Ridge", "Skysand Ridge")
-                                                                                .addChoice("Stumprot Swamp", "Stumprot Swamp")
-                                                                                .addChoice("Sunfang Cliffs", "Sunfang Cliffs")
-                                                                                .addChoice("Sunfang Dawn", "Sunfang Dawn")
-                                                                                .addChoice("Sunstrand Dunes", "Sunstrand Dunes")
-                                                                                .addChoice("Timberslope Grove", "Timberslope Grove")
-                                                                                .addChoice("Timbertop Dale", "Timbertop Dale")
-                                                                                .addChoice("Watchwood Bluffs", "Watchwood Bluffs")
-                                                                                .addChoice("Westweald Shore", "Westweald Shore")),
-                                Commands.slash("edittimer", "Edit a timer for a world boss")
-                                                .addOptions(
-                                                                new OptionData(OptionType.STRING, "map",
-                                                                                "Name of the map", true)
-                                                                                .addChoice("Darkground Swamp", "Darkground Swamp")
-                                                                                .addChoice("Deadvein Gully", "Deadvein Gully")
-                                                                                .addChoice("Deathwisp Sink", "Deathwisp Sink")
-                                                                                .addChoice("Drownfield Wetland", "Drownfield Wetland")
-                                                                                .addChoice("Dryvein Steppe", "Dryvein Steppe")
-                                                                                .addChoice("Farshore Heath", "Farshore Heath")
-                                                                                .addChoice("Flimmerair Steppe", "Flimmerair Steppe")
-                                                                                .addChoice("Hightree Levee", "Hightree Levee")
-                                                                                .addChoice("Longfen Arms", "Longfen Arms")
-                                                                                .addChoice("Longfen Veins", "Longfen Veins")
-                                                                                .addChoice("Longtimber Glen", "Longtimber Glen")
-                                                                                .addChoice("Nightbloom Forest", "Nightbloom Forest")
-                                                                                .addChoice("Rivercopse Fount", "Rivercopse Fount")
-                                                                                .addChoice("Runnelvein Bog", "Runnelvein Bog")
-                                                                                .addChoice("Skysand Ridge", "Skysand Ridge")
-                                                                                .addChoice("Stumprot Swamp", "Stumprot Swamp")
-                                                                                .addChoice("Sunfang Cliffs", "Sunfang Cliffs")
-                                                                                .addChoice("Sunfang Dawn", "Sunfang Dawn")
-                                                                                .addChoice("Sunstrand Dunes", "Sunstrand Dunes")
-                                                                                .addChoice("Timberslope Grove", "Timberslope Grove")
-                                                                                .addChoice("Timbertop Dale", "Timbertop Dale")
-                                                                                .addChoice("Watchwood Bluffs", "Watchwood Bluffs")
-                                                                                .addChoice("Westweald Shore", "Westweald Shore"))
-                                                .addOption(OptionType.STRING, "newtime",
-                                                                "New time for the boss spawn in HH:mm:ss format", true)
-                                                .addOption(OptionType.STRING, "newdate",
-                                                                "New date for the boss spawn in d/MM/yyyy format",
-                                                                true)
-                                                .addOption(OptionType.STRING, "note", "An optional note about the timer", false))
+    public void registerCommands() {
+        OptionData mapOption = getMapOptionData();
 
-                                .queue();
-
-        }
+        jda.updateCommands().addCommands(
+            Commands.slash("aboutme", "Get information about the bot"),
+            Commands.slash("setdesignatedchannel", "Set the designated channel for timers"),
+            Commands.slash("help", "Get a list of available commands and their descriptions"),
+            Commands.slash("addtimer", "Add a timer for a world boss")
+                .addOption(OptionType.STRING, "time", "Time for the boss spawn in HH:mm:ss format", true)
+                .addOptions(mapOption)
+                .addOption(OptionType.STRING, "date", "Date for the boss spawn in dd/MM/yyyy format", false)
+                .addOption(OptionType.STRING, "note", "A note about the timer", false),
+            Commands.slash("deletetimer", "Delete a timer for a world boss")
+                .addOptions(mapOption),
+            Commands.slash("edittimer", "Edit a timer for a world boss")
+                .addOptions(mapOption)
+                .addOption(OptionType.STRING, "newtime", "New time for the boss spawn in HH:mm:ss format", true)
+                .addOption(OptionType.STRING, "newdate", "New date for the boss spawn in d/MM/yyyy format", true)
+                .addOption(OptionType.STRING, "note", "An optional note about the timer", false)
+        ).queue(
+            success -> logger.info("Commands registered successfully"),
+            failure -> logger.error("Command registration failed: ", failure)
+        );
+    }
 }
